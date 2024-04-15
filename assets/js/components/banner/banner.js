e0f8f5e30765a19e6ae8f0e95c421ba1d1dcee2f
鈴木翔也 <suzukishoya@suzukishouyanoMacBook-Air.local>
2024-04-15 23:31:07 +0900

    webkit系のブラウザでも正常に動作するように修正

--- assets/js/components/banner/banner.js
+++ assets/js/components/banner/banner.js
@@ -1,36 +1,35 @@
 const banner = (bannerType, banners) => {
     const interval = 3000;
     const transitionDuration = 1000;
-    const bannerTemplate = document.querySelector(`.banner.${bannerType}`);
+    const bannerContainer = document.querySelector(`.banner-container.${bannerType}`);
     let currentIndex = 0;
 
     const updateBanner = () => {
         const nextIndex = (currentIndex + 1) % banners.length;
         const { img, link } = banners[nextIndex];
+        const newBanner = document.createElement('a');
+        newBanner.href = link;
+        newBanner.classList.add('banner', 'next');
 
         const newImage = new Image();
         newImage.src = `/assets/img/components/banner/${img}.jpg`;
         newImage.alt = img;
-        newImage.classList.add('next');
+        newImage.onload = () => {
+            newBanner.appendChild(newImage);
+            bannerContainer.appendChild(newBanner);
 
-        newImage.addEventListener('load', () => {
-            bannerTemplate.href = link;
-            bannerTemplate.appendChild(newImage);
             setTimeout(() => {
-                const activeImage = bannerTemplate.querySelector('img.active');
-                if (activeImage) {
-                    activeImage.classList.remove('active');
+                const activeBanner = bannerContainer.querySelector('.banner.active');
+                if (activeBanner) {
+                    activeBanner.classList.remove('active');
+                    setTimeout(() => {
+                        activeBanner.remove();
+                    }, transitionDuration);
                 }
-                newImage.classList.add('active');
-                newImage.classList.remove('next');
+                newBanner.classList.add('active');
+                newBanner.classList.remove('next');
             }, 50);
-            setTimeout(() => {
-                const inactiveImage = bannerTemplate.querySelector('img:not(.active)');
-                if (inactiveImage) {
-                    inactiveImage.remove();
-                }
-            }, transitionDuration);
-        });
+        };
 
         currentIndex = nextIndex;
     };
