934b360dffca0084912adfb5d3b7386efa5a9532
鈴木翔也 <suzukishoya@suzukishouyanoMacBook-Air.local>
2024-04-15 23:37:10 +0900

    スマホで上手くいくか試す

--- assets/js/components/banner/banner.js
+++ assets/js/components/banner/banner.js
@@ -1,5 +1,5 @@
 const banner = (bannerType, banners) => {
-    const interval = 3000;
+    const interval = 5000;
     const transitionDuration = 1000;
     const bannerContainer = document.querySelector(`.banner-container.${bannerType}`);
     let currentIndex = 0;
@@ -12,7 +12,7 @@ const banner = (bannerType, banners) => {
         newBanner.classList.add('banner', 'next');
 
         const newImage = new Image();
-        newImage.src = `/assets/img/components/banner/${img}.jpg`;
+        newImage.src = `${window.location.origin}/assets/img/components/banner/${img}.jpg`;
         newImage.alt = img;
         newImage.onload = () => {
             newBanner.appendChild(newImage);
